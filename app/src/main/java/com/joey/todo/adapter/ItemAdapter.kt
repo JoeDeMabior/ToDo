47d0038b852a511fb84502b15b909e90b871e003
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-03-08 14:31:20 +0300

    Add Adapter and RecyclerView

--- app/src/main/java/com/joey/todo/adapter/ItemAdapter.kt
+++ app/src/main/java/com/joey/todo/adapter/ItemAdapter.kt
@@ -1,68 +1,35 @@
 package com.joey.todo.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.CheckBox
-import android.widget.ImageButton
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.joey.todo.R
-import com.joey.todo.model.Item
-import com.joey.todo.my_interface.ItemRowListener
+import com.joey.todo.room.Item
 
-class ItemAdapter(context: Context, items: MutableList<Item>) : BaseAdapter() {
-    private val inflater = LayoutInflater.from(context)
-    private var itemList = items
-    private var rowListener = context as ItemRowListener
+class ItemAdapter : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+    private var items: List<Item> = ArrayList()
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val objectId: String = itemList[position].id as String
-        val itemText: String = itemList[position].text as String
-        val done: Boolean = itemList[position].done as Boolean
-
-        val view: View
-        val holder: ListRowHolder
-        if (convertView == null) {
-            view = inflater.inflate(R.layout.model, parent, false)
-            holder = ListRowHolder(view)
-            view.tag = holder
-        } else {
-            view = convertView
-            holder = view.tag as ListRowHolder
-        }
-
-        holder.label.text = itemText
-        holder.isDone.isChecked = done
-
-        holder.isDone.setOnClickListener {
-            rowListener.modifyItemState(objectId, !done)
-        }
-
-        holder.del.setOnClickListener {
-            rowListener.onItemDelete(objectId)
-        }
-
-        return view
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.model, parent, false)
+        return ItemViewHolder(itemView)
     }
 
-    override fun getItem(position: Int): Any {
-        return itemList[position]
+    override fun getItemCount(): Int {
+        return items.size
     }
 
-    override fun getItemId(position: Int): Long {
-        return position.toLong()
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val currentItem = items[position]
+        holder.txt.text = currentItem.title
+        holder.desc.text = currentItem.description
+        holder.now.text = currentItem.deadline
     }
 
-    override fun getCount(): Int {
-        return itemList.size
+    inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val txt: TextView = itemView.findViewById(R.id.title)
+        val desc: TextView = itemView.findViewById(R.id.desc)
+        val now: TextView = itemView.findViewById(R.id.moment)
     }
-
-    private class ListRowHolder(row: View?) {
-        val label: TextView = row!!.findViewById(R.id.text)
-        val isDone: CheckBox = row!!.findViewById(R.id.status)
-        val del: ImageButton = row!!.findViewById(R.id.clear)
-    }
-
 }
--- app/src/main/java/com/joey/todo/my_interface/ItemRowListener.kt
+++
@@ -1,6 +0,0 @@
-package com.joey.todo.my_interface
-
-interface ItemRowListener {
-    fun modifyItemState(id: String, done: Boolean)
-    fun onItemDelete(id: String)
-}
